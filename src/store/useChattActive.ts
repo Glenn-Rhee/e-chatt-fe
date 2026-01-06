39a9c31bce154045787b7f26c89d077c39470ba8
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-06 23:54:11 +0700

    refactor: change location storage of state useChatStore into localstorage

--- src/store/useChattActive.ts
+++ src/store/useChattActive.ts
@@ -1,4 +1,5 @@
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
+import { createJSONStorage, persist, PersistOptions } from "zustand/middleware";
 
 interface UseChatStore {
   idChatt: string | null;
@@ -7,9 +8,22 @@ interface UseChatStore {
   setIsInformationActive: (v: boolean) => void;
 }
 
-export const useChatStore = create<UseChatStore>((set) => ({
-  idChatt: null,
-  isInformationActive: false,
-  setIsInformationActive: (v) => set({ isInformationActive: v }),
-  setIdChatt: (id) => set({ idChatt: id }),
-}));
+type ChatPersist = (
+  config: StateCreator<UseChatStore>,
+  option: PersistOptions<UseChatStore>
+) => StateCreator<UseChatStore>;
+
+export const useChatStore = create<UseChatStore>(
+  (persist as ChatPersist)(
+    (set) => ({
+      idChatt: null,
+      isInformationActive: false,
+      setIsInformationActive: (v) => set({ isInformationActive: v }),
+      setIdChatt: (id) => set({ idChatt: id }),
+    }),
+    {
+      name: "chatt",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
