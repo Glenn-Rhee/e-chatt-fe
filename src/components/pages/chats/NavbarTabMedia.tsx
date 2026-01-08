233086bd44b0b43bcb450a71385c61a15711faee
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-08 22:12:52 +0700

    refactor: add py-1.5 at Link navbarTabMedia

--- src/components/pages/chats/NavbarTabMedia.tsx
+++ src/components/pages/chats/NavbarTabMedia.tsx
@@ -10,7 +10,7 @@ export default function NavbarTabMedia() {
       <Link
         href={"/chats/media"}
         className={clsx(
-          "text-sm font-semibold text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
+          "text-sm font-semibold py-1.5 text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
           pathname === "/chats/media" ? "after:w-[90%] " : "after:w-0"
         )}
       >
@@ -19,7 +19,7 @@ export default function NavbarTabMedia() {
       <Link
         href={"/chats/links"}
         className={clsx(
-          "text-sm font-semibold text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
+          "text-sm font-semibold py-1.5 text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
           pathname === "/chats/links" ? "after:w-[90%] " : "after:w-0"
         )}
       >
@@ -28,7 +28,7 @@ export default function NavbarTabMedia() {
       <Link
         href={"/chats/documents"}
         className={clsx(
-          "text-sm font-semibold text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
+          "text-sm font-semibold py-1.5 text-neutral-900 w-full flex justify-center relative active:bg-neutral-50 rounded-sm after:block after:content-[''] after:h-0.5 after:bg-neutral-900 after:absolute after:-bottom-1 after:left-1/2 after:right-1/2 after:-translate-x-1/2 after:rounded-md after:transition-all after:duration-200",
           pathname === "/chats/documents" ? "after:w-[90%] " : "after:w-0"
         )}
       >
