33c2c5cb733c7531f9dba2c870a6a5684c42cafe
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-12 19:29:43 +0700

    refactor: change middleware to proxy

---
+++ proxy.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function proxy(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const url = req.nextUrl.pathname;
+
+  if (url.includes("/api")) {
+    if (url.startsWith("/api/auth")) {
+      return NextResponse.next();
+    }
+  } else {
+    if (
+      (url === "/chats" ||
+        url === "/groups" ||
+        url === "/profile" ||
+        url === "/settings") &&
+      !token
+    ) {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
+
+    if ((url.includes("/auth") || url === "/") && token) {
+      return NextResponse.redirect(new URL("/chats", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const matcher = ["/chats"];
