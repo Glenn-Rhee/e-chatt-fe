6d753084a5aa7c95a9f4848a479ed55768ed813a
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-15 21:41:44 +0700

    feat: add validation at birthday user

--- src/components/pages/profile/InputsBirthday.tsx
+++ src/components/pages/profile/InputsBirthday.tsx
@@ -4,6 +4,23 @@ import React, { useRef } from "react";
 export default function InputsBirthday() {
   const inpuutsRef = useRef<(HTMLInputElement | null)[]>([]);
   const maxLengths = [2, 2, 4];
+
+  const today = new Date();
+  const minYear = today.getFullYear() - 12;
+
+  const isLeapYear = (year: number) =>
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  const getMaxDay = (month: number, year?: number) => {
+    if (month === 2) {
+      return year && isLeapYear(year) ? 29 : 28;
+    }
+
+    if ([4, 6, 9, 11].includes(month)) return 30;
+
+    return 31;
+  };
+
   return [0, 1, 2].map((i) => (
     <React.Fragment key={i}>
       <input
@@ -12,6 +29,29 @@ export default function InputsBirthday() {
           inpuutsRef.current[i] = ell;
         }}
         onChange={(e) => {
+          e.target.value = e.target.value.replace(/\D/g, "");
+          const day = Number(inpuutsRef.current[0]?.value);
+          const month = Number(inpuutsRef.current[1]?.value);
+          const year = Number(inpuutsRef.current[2]?.value);
+
+          if (i === 1 && month > 12) {
+            e.target.value = "";
+          }
+
+          if (i === 2 && year > minYear) {
+            e.target.value = "";
+          }
+
+          if (i === 0 && day > 31) {
+            e.target.value = "";
+          }
+
+          if (i === 0 && month) {
+            const maxDay = getMaxDay(month, year);
+            if (day > maxDay) {
+              e.target.value = "";
+            }
+          }
           const value = e.target.value;
 
           if (value.length === maxLengths[i]) {
