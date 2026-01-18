f0bd2083b0a1f30360f56d154b2cb3e808af4d34
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-18 22:07:13 +0700

    feat: added edit image feature

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,10 +1,11 @@
+import { ResponsePayload } from "@/src/types";
 import { getToken } from "next-auth/jwt";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError, UTApi } from "uploadthing/server";
 import z from "zod";
+const utApi = new UTApi();
 
 const f = createUploadthing();
-
 export const ourFileRouter = {
   imageUpload: f({
     image: {
@@ -30,18 +31,37 @@ export const ourFileRouter = {
       if (!input.image) {
         return {
           email: token.email,
+          token: token.token,
         };
       }
 
       const fileKey = input.image.split("/").pop();
 
-      const utApi = new UTApi();
       await utApi.deleteFiles(fileKey!);
 
-      return { email: token.email };
+      return { email: token.email, token: token.token };
     })
     .onUploadComplete(async ({ file, metadata }) => {
       const url = file.ufsUrl;
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_URL + "/user/image",
+        {
+          method: "PATCH",
+          body: JSON.stringify({ imageUrl: url }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: metadata.token!,
+          },
+        },
+      );
+
+      const dataRes = (await res.json()) as ResponsePayload;
+      if (dataRes.status === "failed") {
+        const fileKey = url.split("/").pop();
+
+        await utApi.deleteFiles(fileKey!);
+        throw new UploadThingError(dataRes.message);
+      }
 
       return {
         email: metadata.email,
