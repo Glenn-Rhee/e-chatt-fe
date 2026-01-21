1235775dfcbbca596689e36d7e3bb0f754bcac89
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-21 22:48:43 +0700

    feat: setup socket.io

---
+++ src/lib/socket.ts
@@ -0,0 +1,15 @@
+import { io, Socket } from "socket.io-client";
+let socket: Socket | null = null;
+
+export const connectSocket = (token: string) => {
+  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+    auth: {
+      token,
+    },
+    transports: ["websocket"],
+  });
+
+  return socket;
+};
+
+export const getSocket = () => socket;
