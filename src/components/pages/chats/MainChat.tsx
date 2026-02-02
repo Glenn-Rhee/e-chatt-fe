85e1c0c5aba024cce4cca883d7f733bc78d95801
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-02 19:42:23 +0700

    feat: added label dynamic

--- src/components/pages/chats/MainChat.tsx
+++ src/components/pages/chats/MainChat.tsx
@@ -4,7 +4,7 @@ import IncomingBubble from "./IncomingBubble";
 import OutgoingBubble from "./OutgoingBubble";
 import { useSession } from "next-auth/react";
 import { getFormatTime } from "@/src/helper/getFormatDate";
-import { useEffect, useRef } from "react";
+import { Fragment, useEffect, useRef } from "react";
 import groupMessageByDate from "@/src/helper/groupMessageByDate";
 
 export default function MainChat() {
@@ -22,34 +22,37 @@ export default function MainChat() {
 
   const groupedMessages = groupMessageByDate(message);
 
-  console.log(groupedMessages);
   return (
     <main className="bg-transparent h-full mt-28 space-y-1 py-2 px-3 relative">
       {message && informationsUser && session?.user.userId && (
         <>
-          <div className="w-full my-2 flex justify-center sticky top">
-            <span className="text-white font-medium text-sm px-2 py-1 rounded-md bg-lightblue-500">
-              Today
-            </span>
-          </div>
-          {message.map((msg) =>
-            msg.senderId === session.user.userId ? (
-              <OutgoingBubble
-                idMsg={msg.id}
-                isRead={msg.isRead}
-                key={msg.id}
-                text={msg.content}
-                time={getFormatTime(new Date(msg.createdAt))}
-              />
-            ) : (
-              <IncomingBubble
-                idMsg={msg.id}
-                key={msg.id}
-                text={msg.content}
-                time={getFormatTime(new Date(msg.createdAt))}
-              />
-            ),
-          )}
+          {Object.entries(groupedMessages).map(([dateLabel, msgs]) => (
+            <Fragment key={dateLabel}>
+              <div className="w-full my-2 flex justify-center sticky top">
+                <span className="text-white font-medium text-sm px-2 py-1 rounded-md bg-lightblue-500">
+                  {dateLabel}
+                </span>
+              </div>
+              {msgs.map((msg) =>
+                msg.senderId === session.user.userId ? (
+                  <OutgoingBubble
+                    idMsg={msg.id}
+                    isRead={msg.isRead}
+                    key={msg.id}
+                    text={msg.content}
+                    time={getFormatTime(new Date(msg.createdAt))}
+                  />
+                ) : (
+                  <IncomingBubble
+                    idMsg={msg.id}
+                    key={msg.id}
+                    text={msg.content}
+                    time={getFormatTime(new Date(msg.createdAt))}
+                  />
+                ),
+              )}
+            </Fragment>
+          ))}
         </>
       )}
       <div ref={bottomRef} />
